ace40dc91e5a9e09213454c9b088c4ed2ecc3684
EvergreenNW <99581034+EvergreenNW@users.noreply.github.com>
2023-04-15 17:56:49 +0000

    added initial structure

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Instructions for this Assignment:
 Go to GitHub and create a repo, I suggest calling it P209HW2
 Then using local git, clone that.  
 Open the new folder and add an HTML, CSS, and JS file.
-
 Code the HTML to use a Form.  
 
 The form should have a set of 3 radio buttons where the user can specify a pizza size of small, medium, or large.
--- script.js
+++ script.js
@@ -1,71 +1,38 @@
-// Upon submit:
-//      JS calculates price and show it to user.
-//      Pricing: 
-//          Small $7.00
-//          Medium  $10.00
-//          Large  $14.00
-//          1 topping  $2.00
-//          2 toppings $3.00
-
 document.addEventListener("DOMContentLoaded", function (event) {
-    
-    
-    //Size
-    function handleClick(RadioElement) {   // RadioElement is the object returned from "this"
-        var selectedValue = RadioElement.value;
-        if (selectedValue == "yes") {
-            blah
-        }
-        if (selectedValue == "no") {
-            blah
-        }
-        else {
-            blah   
-        }
-    }
 
-    //Toppings
 
-    //Submit
-    function calculate() {
-        document.querySelector('submit');
+  let total = 0;
+  let totalAmount = document.querySelector('h3');
+  let selectedSize = 0;
+  let selectedToppings = 0;
+  
+  document.addEventListener('click', calculate());
+  
+  function size() {
+    if (button.value === 'small') {
+      let selectedSize = 7;
+    } else if (button.value === 'medium') {
+      let selectedSize = 10;
+    } else {
+      let selectedSize = 14;
+  }
+    
+  function toppings() {
+    if (toppings.value === 'none') {
+      let selectedToppings = 0;
+    } else if (toppings.value === 'one') {
+      let selectedToppings = 2;
+    } else {
+      let selectedToppings = 3;
     }
+  }
+  
+  function calculate() {
+    let total = selectedSize + selectedToppings;
+    console.log (total);
+    totalAmount.textContent = "Your total is: $" + total;
+    document.getElementById(total).innerHTML;
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    });
-
-
-
-
-
-
-
-
-
-
+});
